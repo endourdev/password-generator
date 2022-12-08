67878bf703b3197f5eac07db5f8fbaa439b44ee2
Fofh <ouaoudamohamed448@gmail.com>
2022-12-08 21:00:56 +0100

    Add files via upload

---
+++ google.py
@@ -0,0 +1,25 @@
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+number = "0123456789"
+
+symbols = "#@=$?*/%+-"
+
+total = lower_case + upper_case + number + symbols
+
+lgn = int (input ("Votre Longueur : "))
+
+longeur = lgn
+
+password = "".join(random.sample(total, longeur))
+
+print("Votre mot de passe google est : " + password)
+
+bien = "Si ce script vous à été pratique n'hésitez pas à le partager à vos amis."
+
+input('\n' + bien)
+
+input('\nAppuyer sur entrée ...')
